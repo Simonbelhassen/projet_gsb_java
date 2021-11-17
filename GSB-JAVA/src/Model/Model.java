95d93cfe04db2d0479214c84bebdfd7f9dd2db0e
dimmi <dimmi@DESKTOP-9JMSGJO>
2021-11-17 14:42:48 +0100

    D : Ajouter les dossiers necessaires par default. Entity(ConnexionBDD, FonctionsMetier, IMetier) Model(Model) Vues(PageAcceuil)

---
+++ GSB-JAVA/src/Model/Model.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author dimmi
+ */
+public class Model {
+//    Les pages MODEL sont les pages qui communiquant avec la BDD.
+//    Donc tous les pages dans le package ENTITY qui doivent travailler avec la BDD doivent avoir des connection ici.
+}
